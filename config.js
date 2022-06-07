08b2b412471fc911fa26ac8d477ef14b7f8f545e
simonasablinaite <simonasablinaite@gmail.com>
2022-06-07 15:28:04 +0300

    server-initiated configs

---
+++ config.js
@@ -0,0 +1,34 @@
+console.log('kur dirba kodas?');
+
+console.log(process.env.NODE_ENV);
+
+const config = {};
+
+config.dev = {
+    name: 'dev',
+    passwordLenght: 2,
+    defaultLanguage: 'en',
+    db: {
+        user: 'root',
+        password: 'admin',
+        database: 'batai'
+    },
+}
+
+config.prod = {
+    name: 'prod',
+    passwordLenght: 12,
+    defaultLanguage: 'lt',
+    db: {
+        user: 'node_batai_user',
+        password: 'vcbldc65465165fg65v6f15g21v',
+        database: 'batai-5v542c1v2fs'
+    },
+}
+
+const nodeEnv = process.env.NODE_ENV;
+const env = nodeEnv ? nodeEnv : 'dev';
+const options = config[env] ? config[env] : config.dev;
+
+console.log(nodeEnv);
+console.log(options);
