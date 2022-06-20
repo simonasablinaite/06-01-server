d5457d34754bfe6bee515c848ab01a91b48a5583
simonasablinaite <simonasablinaite@gmail.com>
2022-06-20 13:07:52 +0300

    server: pages

--- pages/404.js
+++ pages/404.js
@@ -1,29 +1,18 @@
-function page404() {
-  return `<!DOCTYPE html>
-  <html lang="en">
+import { PageTemplate } from "../lib/Page.Template.js";
 
-      <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Server</title>
-      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-      <link rel="manifest" href="/favicon/site.webmanifest">
-      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
-      <meta name="msapplication-TileColor" content="#da532c">
-      <meta name="theme-color" content="#ffffff">
-      <link rel="stylesheet" href="/css/base/base.css">
-      </head>
-      
-      <body>
-      <h1>404 page ⛔</h1> 
-      </body>
-                                      
-      </html>`;
+
+class Page404 extends PageTemplate {
+  constructor() {
+    super()
+    this, title = '404 | server';
+  }
+
+  mainHTML() {
+    return `<div class="row">
+                <h1>404 page ⛔</h1>
+            </div>`
+  }
 
 }
 
-export { page404 };
+export { Page404 };
