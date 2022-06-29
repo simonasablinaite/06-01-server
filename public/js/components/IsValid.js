65d1d606ae50aed3ffa2df52e8abccb60f2bd6e7
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-06-29 08:25:47 +0300

    server: data folders

--- public/js/components/IsValid.js
+++ public/js/components/IsValid.js
@@ -106,8 +106,13 @@ class IsValid {
   }
 
   static password(str) {
-    if (str.length < 2) {
-      return [true, 'Per trumpas password tekstas'];
+    const minPasswordLength = 12;
+
+    if (typeof str !== 'string') {
+      return [true, 'Netinkamas tipas, turi buti "string"'];
+    }
+    if (str.length < minPasswordLength) {
+      return [true, `Per trumpas password tekstas, turi buti minimum ${minPasswordLength} simboliai`];
     }
 
     return [false, 'OK'];
